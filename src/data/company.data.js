6629cad65fe590e06ffa2ef3c6ef5fb87091a4bb
csbizz <95736373+csbizz@users.noreply.github.com>
2024-09-30 17:38:21 +0900

    Init

--- src/data/company.data.js
+++ src/data/company.data.js
@@ -1,8 +1,43 @@
 export class CompanyData {
-  constructor(client) {
-    this.data = client.Company; // 이 부분에 각 모델(스키마)를 연결합니다.
-  }
+	constructor(client) {
+		this.data = client.Company; // 이 부분에 각 모델(스키마)를 연결합니다.
+	}
 
-  // 이 아래로 직접 DB와 통신하는 코드를 작성합니다.
-  // 여기서 DB와 통신해 받아온 데이터를 위로(service로) 올려줍니다.
+	// 이 아래로 직접 DB와 통신하는 코드를 작성합니다.
+	// 여기서 DB와 통신해 받아온 데이터를 위로(service로) 올려줍니다.
+	count = async () => {
+		const count = await this.data.count();
+
+		return count;
+	};
+
+	findMany = async () => {
+		const companies = await this.data.findMany();
+
+		return companies;
+	};
+
+	findById = async () => {
+		const company = await this.data.findById();
+
+		return company;
+	};
+
+	create = async () => {
+		const company = await this.data.create();
+
+		return company;
+	};
+
+	update = async () => {
+		const company = await this.data.update();
+
+		return company;
+	};
+
+	delete = async () => {
+		const company = await this.data.delete();
+
+		return company;
+	};
 }
--- src/services/investment.service.js
+++ src/services/investment.service.js
@@ -1,9 +1,39 @@
 export class InvestmentService {
-  constructor(investmentData) {
-    this.data = investmentData; // 이 부분에 data.js를 연결합니다.
-  }
+	constructor(investmentData) {
+		this.data = investmentData; // 이 부분에 data.js를 연결합니다.
+	}
 
-  // 이 아래로 데이터를 가공하는 코드를 작성합니다.
-  // 비즈니스 로직, DB에서 가져온 데이터를 가공하는 코드가 주로 작성됩니다.
-  // 여기서 가공된 데이터를 controller로 올려줍니다.
+	// 이 아래로 데이터를 가공하는 코드를 작성합니다.
+	// 비즈니스 로직, DB에서 가져온 데이터를 가공하는 코드가 주로 작성됩니다.
+	// 여기서 가공된 데이터를 controller로 올려줍니다.
+	getInvestments = async ({ parameter }) => {
+		const totalCount = await this.data.count();
+		const list = await this.data.findMany();
+
+		return { list, totalCount };
+	};
+
+	getInvestment = async ({ parameter }) => {
+		const investment = await this.data.findById();
+
+		return investment;
+	};
+
+	postInvestment = async ({ parameter }) => {
+		const investment = await this.data.create();
+
+		return investment;
+	};
+
+	updateInvestment = async ({ parameter }) => {
+		const investment = await this.data.update();
+
+		return investment;
+	};
+
+	deleteInvestment = async ({ parameter }) => {
+		const investment = await this.data.delete();
+
+		return investment;
+	};
 }
