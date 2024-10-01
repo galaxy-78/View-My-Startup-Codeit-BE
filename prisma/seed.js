a8e39b18ff882e08fdd728d34d6df662d2d6791b
kipid <kipacti@gmail.com>
2024-10-02 08:05:48 +0900

    Update seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,15 +1,29 @@
-import { USER } from '../mock/mock.js';
+import { USER, COMPANY, INVESTMENT } from "../mock/mock.js";
 import { prismaClient as prisma } from '../src/connection/postgres.connection.js';
+import encrypt, { generateRandomHexString, ITER_FULL } from '../src/encrypt.js';
 
 async function main() {
-  await prisma.$transaction([prisma.user.deleteMany()]);
+	await prisma.user.deleteMany();
+	await prisma.company.deleteMany();
+	await prisma.investment.deleteMany();
 
-  await prisma.$transaction([
-    prisma.user.createMany({
-      data: USER,
-      skipDuplicates: true,
-    }),
-  ]);
+	await Promise.all(
+		USER.map(async user => {
+			user.salt = generateRandomHexString();
+			user.pwdEncrypted = encrypt(user.salt, user.password, ITER_FULL);
+			delete user.password;
+			user.iter = ITER_FULL - 1;
+			await prisma.user.create({ data: user });
+		})
+	);
+	// await prisma.company.createMany({
+	// 	data: COMPANY,
+	// 	skipDuplicates: true,
+	// });
+	// await prisma.investment.createMany({
+	// 	data: INVESTMENT,
+	// 	skipDuplicates: true,
+	// })
 }
 
 main()
@@ -17,7 +31,7 @@ main()
     await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.log(e);
+    console.error(e);
     await prisma.$disconnect();
     process.exit(1);
   });
