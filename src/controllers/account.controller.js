e19c9680c4e17903f7148764695d9ead722be064
kipid <kipacti@gmail.com>
2024-10-08 23:32:40 +0900

    Update account.controller.js
    Comment deprecated

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -13,15 +13,15 @@ export class AccountController {
 	// 따라서 superstruct나 validation 관련 코드는 여기에 많이 작성됩니다.
 	// validation은 위쪽 router에서도 사용되고는 하는데, 이에 대해서는 그쪽에 주석 남기겠습니다.
 	// 응답의 status를 지정하고, body를 전달합니다.
-	postLogin = async (req, res) => {
-		const { email, pwdEncrypted } = req.body;
-		const user = await this.service.getUser({ email });
-		const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-		if (encryptRest(user.salt, pwdEncrypted, user.iter) === user.pwdEncrypted) {
-			const session = await this.service.updateUserIterAndCreateSession(user, ip);
-			res.json(session);
-		}
-	};
+	// postLogin = async (req, res) => {
+	// 	const { email, pwdEncrypted } = req.body;
+	// 	const user = await this.service.getUser({ email });
+	// 	const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+	// 	if (encryptRest(user.salt, pwdEncrypted, user.iter) === user.pwdEncrypted) {
+	// 		const session = await this.service.updateUserIterAndCreateSession(user, ip);
+	// 		res.json(session);
+	// 	}
+	// };
 
 	postLogout = async (req, res) => {
 		assert(req.body, ssnBodyWithPwdEncrypted);
@@ -51,19 +51,16 @@ export class AccountController {
 		res.json(session);
 	};
 
-	// TODO del this for security in production mode.
-	getUsers = async (req, res) => {
-		const users = await this.service.getUsers();
+	// getUsers = async (req, res) => {
+	// 	const users = await this.service.getUsers();
+	// 	res.json(users);
+	// };
 
-		res.json(users);
-	};
-
-	postPwdIter = async (req, res) => {
-		const { email } = req.body;
-		const account = await this.service.postPwdIter({ email });
-
-		res.json(account);
-	};
+	// postPwdIter = async (req, res) => {
+	// 	const { email } = req.body;
+	// 	const account = await this.service.postPwdIter({ email });
+	// 	res.json(account);
+	// };
 
 	postSsnIter = async (req, res) => {
 		assert(req.body, ssnBody);
