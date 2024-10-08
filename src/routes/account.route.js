95599f922df7182565a6067923fd42cb9d6ea9c0
kipid <kipacti@gmail.com>
2024-10-09 00:42:27 +0900

    Update account.route.js
    /log-out and /log-out-from-all to userSessionController .postLogout and .postLogoutFromAll

--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -9,17 +9,17 @@ export const accountRouter = express.Router(); // export 해주어야 합니다.
 // 여기서 controller의 method를 불러줍니다.
 // 메소드의 리턴값이 아니라 메소드 자체를 넘겨주어야 합니다.
 // app.get이 아닙니다. router.get입니다.
-// accountRouter.post('/log-out', userSessionController.postLogout);
 accountRouter.post('/log-in', authController.postLogin);
 accountRouter.post('/sign-up', authController.postSignup);
 accountRouter.post('/check', userController.postCheck);
 accountRouter.post('/iter', userController.postPwdIter);
 accountRouter.post('/session-iter', userSessionController.postSsnIter);
+accountRouter.post('/log-out', userSessionController.postLogout);
+accountRouter.post('/log-out-from-all', userSessionController.postLogoutFromAll);
 accountRouter.get('/users', userController.getUsers);
 
 
 
-accountRouter.post('/log-out', accountController.postLogout);
 accountRouter.post('/log-out-from-all', accountController.postLogoutFromAll);
 accountRouter.post('/sessions', accountController.postSsns);
 // accountRouter.post('/log-in', accountController.postLogin);
@@ -27,4 +27,5 @@ accountRouter.post('/sessions', accountController.postSsns);
 // accountRouter.post('/check', accountController.postCheck);
 // accountRouter.post('/iter', accountController.postPwdIter);
 // accountRouter.post('/session-iter', accountController.postSsnIter);
+// accountRouter.post('/log-out', accountController.postLogout);
 // accountRouter.get('/users', accountController.getUsers);
